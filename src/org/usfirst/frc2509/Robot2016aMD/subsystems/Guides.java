e1094e675ef4ed35fa4687b1579b6c2fd6e1ee37
Robotics <Robotics>
2017-01-07 15:57:40 -0600

    Error version, NEED TO FIXgit commit!

---
+++ src/org/usfirst/frc2509/Robot2016aMD/subsystems/Guides.java
@@ -0,0 +1,21 @@
+
+
+package org.usfirst.frc2509.Robot2016aMD.subsystems;
+
+import org.usfirst.frc2509.Robot2016aMD.RobotMap;
+import org.usfirst.frc2509.Robot2016aMD.commands.*;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+/**
+ *
+ */
+public class Guides extends Subsystem {
+
+
+    public void initDefaultCommand() {
+        
+    }
+}
+
